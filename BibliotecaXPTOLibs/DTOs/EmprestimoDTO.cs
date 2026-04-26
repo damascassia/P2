8d34154c4eedf4d4b0290248fe2dc0aa3637da9f
Cássia Damas <cassiadamassilva@gmail.com>
2026-04-26 15:28:07 +0100

    Testes finais

---
+++ BibliotecaXPTOLibs/DTOs/EmprestimoDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaXPTOLibs.DTOs
+{
+    public class EmprestimoDTO
+    {
+        public string LeitorDoc { get; set; }   
+        public int ExemplarId { get; set; }     
+    }
+
+
+    public class DevolucaoDTO
+    {
+        public int ExemplarId { get; set; }     
+    }
+
+    public class EmprestimoAtivoDTO
+    {
+        public int Id { get; set; }
+        public string StatusEmprestimo { get; set; }  
+    }
+}
+
---
+++ BibliotecaXPTOLibs/Repositories/Interfaces/IEmprestimosRepository.cs
@@ -0,0 +1,16 @@
+﻿using BibliotecaXPTOLibs.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaXPTOLibs.Repositories.Interfaces
+{
+    public interface IEmprestimosRepository
+    {
+        void RealizarRequisicao(string leitorDoc, int exemplarId, string tagRepo);
+        void RealizarDevolucao(int exemplarId, string tagRepo);
+        List<SituacaoEmprestimoDTO> ObterSituacaoLeitor(int leitorId, string tagRepo);
+    }
+}
